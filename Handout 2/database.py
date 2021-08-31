29253f6a29ec4e8b9173e800a36664f5383c8bda
Guilherme Rameh <guilhermedr@al.insper.edu.br>
2021-08-31 09:15:22 -0300

    Handout 2 completo. Commit final

--- Handout 2/database.py
+++ Handout 2/database.py
@@ -1,7 +1,42 @@
 import sqlite3
+from dataclasses import dataclass
+
+
+@dataclass
+class Note:
+    id: int = None
+    title: str = None
+    content: str = ''
+
 
 class Database:
 
     def __init__(self, name):
         self.conn = sqlite3.connect(name+".db")
         self.conn.execute("CREATE TABLE IF NOT EXISTS note (id INTEGER PRIMARY KEY, title TEXT, content TEXT NOT NULL);")
+
+
+    def add(self, note):
+        title = note.title
+        content = note.content
+        self.conn.execute(f"INSERT INTO note (title, content) VALUES ('{title}', '{content}');")
+        self.conn.commit()
+
+    def get_all(self):
+        lista = []
+        cursor = self.conn.execute("SELECT id, title, content FROM note")
+        for linha in cursor:
+            id = linha[0]
+            title = linha[1]
+            content = linha[2]
+            lista.append(Note(id=id, title=title, content=content))
+        return lista
+
+    def update(self, entry):
+        self.conn.execute(f"UPDATE note SET title = '{entry.title}' WHERE id={entry.id}")
+        self.conn.execute(f"UPDATE note SET content = '{entry.content}' WHERE id={entry.id}")
+        self.conn.commit()
+
+    def delete(self, note_id):
+        self.conn.execute(f"DELETE FROM note WHERE id={note_id};")
+        self.conn.commit()
--- Handout 2/exemplo_de_uso.py
+++ Handout 2/exemplo_de_uso.py
@@ -1,3 +1,15 @@
-from database import Database
+from database import *
 
 db = Database('banco')
+
+# db.add(Note(title='Pão doce', content='Abra o pão e coloque o seu suco em pó favorito.'))
+# db.add(Note(title=None, content='Lembrar de tomar água'))
+
+notes = db.get_all()
+for note in notes:
+    print(f'Anotação {note.id}:\n  Título: {note.title}\n  Conteúdo: {note.content}\n')
+
+update = notes[1]
+update.title = 'Pão salgado'
+update.content = 'ha, thats pog champ'
+db.update(update)
